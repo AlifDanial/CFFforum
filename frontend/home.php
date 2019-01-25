0e595573382d5bd6475e8c050ccd7f6d744c29fa
Muhammad Alif Danial <aldmnsri@gmail.com>
2019-01-25 16:30:27 +0800

    Frontend touch-ups
    changed fonts to resemble xd prototype, changed hover properties of vote , answer and view.

--- frontend/home.php
+++ frontend/home.php
@@ -17,13 +17,52 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <!-- Bootstrap core CSS -->
 <link href="bootstrap.min.css" rel="stylesheet">
 
-
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
 <link href="style.css" rel="stylesheet">
 
 <style>
-
+.card-title{
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+  padding-left: 30px;
+  text-align: right;
+  color: rgb(0, 0, 0);
+}
+.card-link{
+  font-family: 'Montserrat', sans-serif;
+  font-size: 20px;
+}
+.thread-button{
+  font-family: 'Montserrat', sans-serif;
+}
+.card-body1{
+  padding-left:210px;
+}
+.nav-bar{
+  width: 200px;
+  position: fixed;
+  z-index: 1;
+  top: 200px;
+  left: 30px;
+  background: transparent;
+  overflow-x: hidden;
+  padding: 10px 10px;
+}
+.nav-bar a {
+  padding: 10px 20px 10px 20px;
+  text-decoration: bold;
+  font-size: 20px;
+  color: rgb(0, 0, 0);
+  display: block;
+}
+.nav-bar a:hover {
+  color: rgb(60, 186, 84);
+}
+.nav-link{
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+}
 </style>
 </head>
 <body>
@@ -54,8 +93,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
     <br />
 
-    <div class="sidenav">
-      <a href="home.php" class="nav-link">Home</a>
+    <div class="nav-bar">
+      <a href="home.php" class="nav-link" >Home</a>
       <a href="discussion.php" class="nav-link">Discussions</a>
       <a href="crops.php" class="nav-link">Crops</a>
       <a href="tags.php" class="nav-link">Tags</a>
@@ -77,7 +116,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <?php
   include_once("config.php");
   //to implement thread limit and next pages
-  $sql = "SELECT * FROM thread ORDER BY ThreadID LIMIT 7";
+  $sql = "SELECT * FROM thread ORDER BY ThreadID LIMIT 5";
   $res = mysqli_query($link,$sql);
   $threads = "";
   if(mysqli_num_rows($res) > 0){
@@ -89,30 +128,29 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       $votecount = $row['ThreadVoteCount'];
       $answercount = $row['ThreadAnswersCount'];
       $threads .= "
-              <div class='card mb-4'>
-                <div class='card-body '>  
+              <div class='card mb-4 '>
+                <div class='card-body'>  
                   <div class='row'>
 
-                      <div class='text-center col-md-0 pl-2 pt-1'>
-                    <a href='#' class='text-center btn disabled pl-1 pr-1' text-align='center'>".$votecount."</a>
+                      <div class='text-center col-md-0 pl-2 pt-1' >
+                    <a href='specificquestion.php?cthreadID=".$threadID." '' class='card-link'  style='text-decoration: none' >".$votecount."</a>
                     <p class='thread-button pl-1 pr-1'>Votes</p>
                       </div>
                       <div class='text-center col-md-0 pl-2 pt-1 '>
-                    <a href='#' class='btn disabled pl-1 pr-1'>".$viewcount."</a>
+                    <a href='specificquestion.php?cthreadID=".$threadID." '' class='card-link' style='text-decoration: none'>".$viewcount."</a>
                     <p class='thread-button pl-1 pr-1'>Views</p>
                       </div>
                       <div class='text-center col-md-0 pl-2 pt-1'>
-                    <a href='#' class='btn disabled pl-1 pr-1'>".$answercount."</a> 
+                    <a href='specificquestion.php?cthreadID=".$threadID." '' class='card-link' style='text-decoration: none'>".$answercount."</a> 
                     <p class='thread-button pl-1 pr-1'>Answers</p>
                       </div>
-
                   </div>
 
                     <font size='-1'>
                     <!--<p class='thread-button'>Votes&nbsp; Views&nbsp; Answers </p>-->
                     </font>
-                    <div class='card-body1 pt-1 pb-0'>
-                        <h4><a href='specificquestion.php?cthreadID=".$threadID." class='btn disabled'>".$title."</a></h4>
+                    <div class='card-body1 pt-0 pb-0 '>
+                        <h4><a href='specificquestion.php?cthreadID=".$threadID." '' class='card-title' style='text-decoration: none' >".$title."</a></h4>
                             </div>
                 </div>
              </div>";
@@ -143,7 +181,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       <form action="/action_page.php">
       <br>
       <center>
-      <textarea name="questiontitle" placeholder="Title" rows="1" cols="60" style="font-size: 15px;"></textarea>
+      <textarea name="questiontitletitle" placeholder="Title" rows="1" cols="60" style="font-size: 15px;"></textarea>
       <br>
       <br> 
       <textarea name="questiontags" placeholder="Tags" rows="1" cols="60" style="font-size:15px;"></textarea>
