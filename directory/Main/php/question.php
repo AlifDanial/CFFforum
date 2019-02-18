0de3cfd6ee92fff01f841c072833d88f5ef50a4e
Muhammad Alif Danial <aldmnsri@gmail.com>
2019-02-18 16:21:41 +0800

    Upvote Limit

--- directory/Main/php/question.php
+++ directory/Main/php/question.php
@@ -8,8 +8,15 @@
         include_once "config.php";
         mysqli_query($link,"SELECT ThreadSubject,ThreadDescription,ThreadUserID FROM thread");
         $result = mysqli_query($link,"INSERT into thread (ThreadSubject,ThreadDescription,ThreadUserID) VALUES ('".$title."','".$content."','".$uid."')");
+        
+        $thread = mysqli_query($link,"SELECT ThreadID From thread Where ThreadUserID = '".$uid."' And ThreadDescription = '".$content."' And ThreadSubject = '".$title."'");;
+        $threadid = $thread->fetch_assoc();
+      (int) $ThreadID = $threadid['ThreadID'];
+        mysqli_query($link,"SELECT ThreadID From thread_votes");
+        $result1 = mysqli_query($link,"INSERT INTO thread_votes(ThreadID)
+                                       SELECT ThreadID FROM thread WHERE ThreadID = '".$ThreadID."'");
         mysqli_close($link);
-        if($result){
+        if($result && $result1){
             header("location: home.php");
         }
         else{
